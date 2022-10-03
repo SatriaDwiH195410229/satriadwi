edc3339a99370bf2772c473bac854c306ef5a7a1
SatriaDwiH195410229 <satriadwi1278@gmail.com>
2022-10-03 13:03:45 +0700

    menambahkan file web statis

---
+++ Web/bio.js
@@ -0,0 +1,1 @@
+alert("BIODATA SAYA\n\nNAMA       : Satria Dwi H\n\nTTL            : Bantul, 12 Mei 2001\n\nLokasi        : Yogyakarta\n\nIG               : @wiisatria\n\nFB              : Satria(Marsha)");
